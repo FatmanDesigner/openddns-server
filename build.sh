f2d95977d4dae7318410040d688a56e2026fb460
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2017-10-28 08:07:48 +0800

    Support platform specific builds: Darwin and Linux

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Building Open DDNS Server..."
-go build -o bin/openddnsd src/*
+env GOOS=darwin GOARCH=amd64 go build -o bin/openddnsd-darwin src/*
+env GOOS=linux GOARCH=amd64 go build -o bin/openddnsd-linux src/*
 
 echo "Done!"
